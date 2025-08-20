987e70103b1646e59889cbda3b172a683e571f3b
husainsalah <husain.salah@gmail.com>
2025-08-20 17:45:07 -0400

    reset modules

---
+++ frontend/components/router.vue
@@ -0,0 +1,45 @@
+<script>
+import { createWebHistory, createRouter } from 'vue-router'
+import Index from '../pages/index.vue'
+import Music from '../pages/discog.vue'
+import Press from '../pages/press.vue'
+import Contact from '../pages/contact.vue'
+import Artist from '../pages/artists.vue'
+
+import Books from '../pages/books.vue'
+import Video from '../pages/video.vue'
+import ReleaseDet from './releaseDet.vue'
+
+const history = createWebHistory()
+const routes = [
+  {
+    path: '/',
+    component: Index,
+  },
+  {
+    path: '/discog',
+    component: Music,
+  },
+{
+    path: '/press',
+    component: Press,
+    },
+    {
+        path: '/contact',
+    component: Contact
+    },
+  {
+    path: '/artist',
+    component: Artist,
+  },
+  
+
+]
+
+const router = createRouter({
+  history,
+  routes,
+})
+
+export default router
+</script>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -24,11 +24,7 @@ export default defineNuxtConfig({
     dataset: process.env.NUXT_SANITY_DATASET || "production",
     apiVersion: process.env.NUXT_SANITY_API_VERSION || "2025-04-01",
     token: process.env.NUXT_SANITY_API_READ_TOKEN, // Only required when using a private dataset
-    visualEditing: {
-      token: process.env.NUXT_SANITY_API_READ_TOKEN || "",
-      studioUrl: process.env.NUXT_SANITY_STUDIO_URL,
-      zIndex: 51,
-    },
+
   },
   runtimeConfig: {
     protectedPassword: process.env.PROTECTED_PASSWORD || 'press',
